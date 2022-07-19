d1aeed0285e69d3a77770c0434d7a1707b6a7067
lmoshood <lawalmoshood80@gmail.com>
2022-07-19 19:35:18 +0100

    complete product fetching from db

--- product.php
+++ product.php
@@ -6,20 +6,21 @@ if(isset($_POST["addproduct"]))
   $price = $_POST["price"];
   $quantity = $_POST["quantity"];
 
-
+  var_dump($_FILES["image"]);
   $image = $_FILES["image"]["name"];
   $tmp_dir = $_FILES["image"]["tmp_name"];
   $imageSize = $_FILES["image"]["size"];
   $imgExt = strtolower(pathinfo($image, PATHINFO_EXTENSION));
 
-  $image = $image.".".$imgExt;
+  $nimage = uniqid(rand(), true).".".$imgExt;
   $upload_dir = 'images/';
+
   if($imageSize < 5000000){
-    move_uploaded_file($tmp_dir, $upload_dir.$image);
+    move_uploaded_file($tmp_dir, $upload_dir.$nimage);
   }
 
   $sql = "INSERT INTO product(productname, price, quantity, image) 
-  values('$productname', '$price', '$quantity', '$image')";
+  values('$productname', '$price', '$quantity', '$nimage')";
   
   
   if($db->query($sql)){
@@ -37,6 +38,14 @@ if(isset($_POST["addproduct"]))
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<style>
+  img {
+    width: 50px;
+    height: 50px;
+  }
+</style>
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.css"/>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.12.1/datatables.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </head>
@@ -58,7 +67,7 @@ if(isset($_POST["addproduct"]))
       </div>
       <div class="modal-body">
         <!-- Form starts here -->
-        <form method="post">
+        <form method="post" enctype="multipart/form-data">
   <div class="form-group">
     <label>Product Name</label>
     <input type="text" class="form-control" name="productname"  placeholder="Enter product name">
@@ -92,33 +101,36 @@ if(isset($_POST["addproduct"]))
 </div>
 <!-- Modal end -->
 <!-- Table starts -->
-<table class="table table-striped table-hover">
+<table class="table table-striped table-hover" id="example">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">S/N</th>
+      <th scope="col">Product Name</th>
+      <th scope="col">Price (#)</th>
+      <th scope="col">Quantity</th>
+      <th scope="col">Image</th>
+      <th scope="col">Option</th>
     </tr>
   </thead>
   <tbody>
+    <?php
+    $sql = "SELECT * FROM PRODUCT";
+    $result = $db->query($sql);
+    foreach($result as $row){
+      ?>
     <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+      <td><?php echo $row["productname"]; ?></td>
+      <td><?php echo $row["price"]; ?></td>
+      <td><?php echo $row["quantity"]; ?></td>
+      <td>
+   <img src="images/<?php echo $row["image"]; ?>"  class="img-fluid" alt="">
+      </td>
+      <td>Edit/ Delete</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+    <?php
+    }
+   ?>
   </tbody>
 </table>
 <!-- Table end -->
@@ -130,5 +142,20 @@ if(isset($_POST["addproduct"]))
     
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+
+ 
+
+<script>
+$(document).ready(function () {
+    $('#example').DataTable();
+});
+</script>
 </body>
 </html>
+
+
+
+
+
+
+
